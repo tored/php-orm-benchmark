abb2011c6024a1461b6dee38419ba82636b05a35
Michal Piotrowski <michal@eventhorizon.pl>
2012-04-02 22:00:04 +0200

    fix memory calculation in doctrine 2.1 benachmark

--- doctrine_21/TestRunner.php
+++ doctrine_21/TestRunner.php
@@ -1,49 +1,11 @@
 <?php
 
-require dirname(__FILE__) . '/Doctrine21TestSuite.php';
-$time = microtime(true);
-$memory = memory_get_usage();
-$test = new Doctrine21TestSuite();
-$test->initialize();
-$test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
-unset($test);
-
-require dirname(__FILE__) . '/Doctrine21WithCacheTestSuite.php';
-$time = microtime(true);
-$memory = memory_get_usage();
-$test = new Doctrine21WithCacheTestSuite();
-$test->initialize();
-$test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
-unset($test);
+passthru('php '.dirname(__FILE__).'/TestRunnerDoctrine21TestSuite.php');
+passthru('php '.dirname(__FILE__).'/TestRunnerDoctrine21WithCacheTestSuite.php');
 
 // Optional tests of the alternative abstraction levels of results doctrine provides.
 // These are often used in production when data is only needed for presentation (read-only) purposes.
 
-require dirname(__FILE__) . '/Doctrine21ArrayHydrateTestSuite.php';
-$time = microtime(true);
-$memory = memory_get_usage();
-$test = new Doctrine21ArrayHydrateTestSuite();
-$test->initialize();
-$test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
-unset($test);
-
-require dirname(__FILE__) . '/Doctrine21ScalarHydrateTestSuite.php';
-$time = microtime(true);
-$memory = memory_get_usage();
-$test = new Doctrine21ScalarHydrateTestSuite();
-$test->initialize();
-$test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
-unset($test);
-
-require dirname(__FILE__) . '/Doctrine21WithoutProxiesTestSuite.php';
-$time = microtime(true);
-$memory = memory_get_usage();
-$test = new Doctrine21WithoutProxiesTestSuite();
-$test->initialize();
-$test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
-unset($test);
+passthru('php '.dirname(__FILE__).'/TestRunnerDoctrine21ArrayHydrateTestSuite.php');
+passthru('php '.dirname(__FILE__).'/TestRunnerDoctrine21ScalarHydrateTestSuite.php');
+passthru('php '.dirname(__FILE__).'/TestRunnerDoctrine21WithoutProxiesTestSuite.php');
