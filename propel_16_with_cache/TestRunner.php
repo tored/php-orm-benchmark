2fcc4b3dd1f189fd7d93b54d4bea8b58fca78f0f
Michal Piotrowski <michal@eventhorizon.pl>
2012-04-02 22:24:59 +0200

    fix memory calculation in propel 1.6 benchmark

--- propel_16_with_cache/TestRunner.php
+++ propel_16_with_cache/TestRunner.php
@@ -3,8 +3,8 @@
 require dirname(__FILE__) . '/Propel16WithCacheTestSuite.php';
 
 $time = microtime(true);
-$memory = memory_get_usage();
+$memory = memory_get_usage(true);
 $test = new Propel16WithCacheTestSuite();
 $test->initialize();
 $test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
+echo sprintf(" %11d | %6.2f |\n", (memory_get_usage(true) - $memory), (microtime(true) - $time));
