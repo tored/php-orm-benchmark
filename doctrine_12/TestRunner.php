2c223f3493e8d39d179e0dba2c2b5e66e1e43eb0
Michal Piotrowski <michal@eventhorizon.pl>
2012-04-02 22:16:49 +0200

    fix memory calculation in doctrine 1.2 benchmark

--- doctrine_12/TestRunner.php
+++ doctrine_12/TestRunner.php
@@ -1,19 +1,4 @@
 <?php
 
-require dirname(__FILE__) . '/Doctrine12TestSuite.php';
-
-$time = microtime(true);
-$memory = memory_get_usage();
-$test = new Doctrine12TestSuite();
-$test->initialize();
-$test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
-
-require dirname(__FILE__) . '/Doctrine12WithCacheTestSuite.php';
-
-$time = microtime(true);
-$memory = memory_get_usage();
-$test = new Doctrine12WithCacheTestSuite();
-$test->initialize();
-$test->run();
-echo "memory=".(memory_get_usage() - $memory)." bytes\t time=".(microtime(true) - $time)." seconds\n";
+passthru('php '.dirname(__FILE__).'/TestRunnerDoctrine12TestSuite.php');
+passthru('php '.dirname(__FILE__).'/TestRunnerDoctrine12WithCacheTestSuite.php');
